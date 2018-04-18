4e096f0f531178abafbc99c31f1e26af6971bd44
Andrejs Olins <andrejs.olins@gmail.com>
2018-04-19 00:18:55 +0300

    STEP 1 shopping cart: green test

--- src/main/scala/aolins/oranapp/ShoppingCart.scala
+++ src/main/scala/aolins/oranapp/ShoppingCart.scala
@@ -1,5 +1,5 @@
 package aolins.oranapp
 
 class ShoppingCart(items:List[Item]) {
-  def total:BigDecimal = 0
+  def total:BigDecimal = items.foldLeft(0:BigDecimal)((priceAcc, item) => priceAcc + item.price)
 }
